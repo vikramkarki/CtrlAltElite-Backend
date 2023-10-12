38de05c7e722693e51df7d59172d346280980239
Vikram Karki <vikram@97-64-180-90.mcomdc.com>
2023-10-12 11:48:37 +0530

    updated the models

--- restaurant_review/admin.py
+++ restaurant_review/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from .models import Restaurant, Review
+from .models import Restaurant, Review, Client, Therapist, Appointment, Admin
 
 # Register your models here.
 
 admin.site.register(Restaurant)
 admin.site.register(Review)
+
+admin.site.register(Client)
+admin.site.register(Review)
+admin.site.register(Therapist)
+admin.site.register(Appointment)
+admin.site.register(Restaurant)
+admin.site.register(Admin)
