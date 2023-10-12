6479797235b3301c8b0e4791a9ca79f6a0a29a5a
Vikram Karki <vikram@Vikrams-MacBook-Air.local>
2023-10-12 12:20:23 +0530

    admin update3

--- restaurant_review/admin.py
+++ restaurant_review/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Restaurant, Review, Client, Therapist, Appointment, Admin
+from .models import Restaurant, Review, Client, Therapist, Appointment
 
 # Register your models here.
 
@@ -9,5 +9,3 @@ admin.site.register(Review)
 admin.site.register(Client)
 admin.site.register(Therapist)
 admin.site.register(Appointment)
-admin.site.register(Restaurant)
-admin.site.register(Admin)
